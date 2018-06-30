f627573701816e6bb3c4e3e28b1f94dd9ce17093
MiriamPeskowitz <miriam5000@gmail.com>
2018-06-30 10:01:37 -0400

    static controller home page added

--- app/controllers/soupkitchens_controller.rb
+++ app/controllers/soupkitchens_controller.rb
@@ -1,2 +1,57 @@
 class SoupkitchensController < ApplicationController
+
+
+  def index
+  	@soupkitchens = Soupkitchen.all
+  end 
+
+  def new
+  	@soupkitchen = Soupkitchen.new
+  end
+
+  
+  def create 
+  	@soupkitchen = SoupKitchen.new(soup_kitchen_params)
+  	if @soupkitchen.save
+  		redirect_to soup_kitchen_path(@soup_kitchen), notice: "New SoupKitchen was successfully added."
+  	else 
+  		flash[:notice] = "Something went wrong"
+  		render :new
+  	end 
+  end
+
+  def show
+    @comments = Comment.all
+    @soupkitchens = SoupKitchen.all
+  end
+
+  def edit 
+  end
+
+  def update
+  	@soupkitchen.update(soup_kitchen_params)
+  	if @soupkitchen.save
+  		redirect_to soup_kitchen_path(@soup_kitchen), notice: "Changes to #{@soupkitchen} were successful."
+  	else 
+  		flash[:notice] = "Something went wrong"
+  		render :edit
+  	end 
+  end 
+
+
+   def destroy
+  	@soupkitchen.destroy
+  	redirect_to @soupkitchens_path
+  end 
+
+ private
+  def set_soupkitchen
+	  @soupkitchen = Soupkitchen.find(params[:id])
+  end 
+
+
+  def soupkitchen_params
+  	params.require(:soupkitchen).permit(:name, :address, :city, :state, :zipcode, :phone, :indoors)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'static/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+
   resources :users
   resources :soupkitchens do
   	resources :comments
