31a299ff0056e02128343bca70fe1e092d3b195d
MiriamPeskowitz <miriam5000@gmail.com>
2018-07-01 11:33:25 -0400

    created nav bar

--- app/models/soupkitchen.rb
+++ app/models/soupkitchen.rb
@@ -2,7 +2,6 @@ class Soupkitchen < ApplicationRecord
 	has_many :users
 	# has_many :soupkitchen_comments
 	has_many :comments
-  # , through: :soupkitchen_comments
 	accepts_nested_attributes_for :comments, :allow_destroy => true 
 	# , reject_if proc { |att| att['name'].blank? }
 	# try: :reject_if => :all_blank
