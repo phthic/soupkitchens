29e80e59609ed1ca1a13eb34e1894154b6c84319
MiriamPeskowitz <miriam5000@gmail.com>
2018-07-13 13:54:17 -0400

    added some vaildations, added password confirmation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :soupkitchen
 
-	# validates_presence_of :title, :content
+	validates_presence_of :title, :content
+
+  # make this into a scope method == pluralize(@soupkitchen.comments.count, "comment")%> 
 end
 
