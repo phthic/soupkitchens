a1d9cb1655453862a8dd827c14db51060e592f6e
MiriamPeskowitz <miriam5000@gmail.com>
2018-07-15 10:59:58 -0400

    debugging omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,33 +4,51 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+# oath2 flow
+# 1. user goes to /auth/github on my site
+# 2. omniauth rdirects them to gh, providing gh with the key and secret that identifies my app, so gh knows about it. 
+# 3. User logs in/authenticates with GH
+# 4. GH redirects them back to my app -- this is the callback URL -- and provides my app with a secret code that represents the user on github. App doesn't yet know the data. 
+#5. my app sends that secret code back to github
+# 6. GH confirms that the code came from gh and that my app received it. 
+# # 7. GH sends back the user data. It's a 2-step handshake. Prevents "man in the middle attack".
+# 8. I check if the user exists in my system, if so, log them in 
+# 7. otherwise, create a user based on their email. 
+
+# -- using devise with omniauth abstracts all of this. 
+
+  # raise auth_hash.inspect
 
   def create
-    binding.pry
+ 
     if auth_hash = request.env["omniauth.auth"]
-      # raise auth_hash.inspect
-      oauth_email = request.env["omniauth.auth"]["info"]["email"]
+      user = User.find_or_create_by_omniauth(auth_hash)
+       session[:user_id] = user.id
+        redirect_to root_path
+    
+      # This whole branch can be refactored through .first_or_create 
+      # oauth_email = request.env["omniauth.auth"]["info"]["email"]
       # if user = User.find_by(:email => oauth_email) #if your system knows this person 
       
       #    session[:user_id] = @user.id
       #    redirect_to root_path
-      #  else
+       # else
        
-      #   # create a user, we know they are but it's their first time here. 
-      #    user = User.new(:email => oauth_email, password => SecureRandom.hex)
-      #    if user.save
-      #       session[:user_id] = @user.id
-      #       redirect_to root_path
-      #     else
-      #       raise user.errors.full_messages.inspect
-      #     end
-      #  end
+       #  # create a user, we know they are but it's their first time here. 
+       #   user = User.new(:email => oauth_email, password => SecureRandom.hex)
+       #   if user.save
+       #      session[:user_id] = @user.id
+       #      redirect_to root_path
+       #    else
+       #      raise user.errors.full_messages.inspect
+       #    end
+       # end
 
-    else  
-      user = User.find_by(first_name: params[:user][:first_name]) 
+    else   #if regular login 
+      user = User.find_by(email: params[:user][:email]) 
         if user && user.authenticate(params[:user][:password]) 
-         session[:user_id] = @user.id
-         flash.now[:success] = "Welcome #{@user.first_name}."
+         session[:user_id] = user.id
+         # flash.now[:success] = "Welcome #{@user.first_name}."
          redirect_to root_path
  
         else 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,39 @@ class User < ApplicationRecord
   validates  :first_name, presence: true, length: {maximum: 20}
   validates :last_name, presence: true, _confirmation: true, length: {maximum: 20}
   validates :password, presence: true, length: {minimum: 8}
- 
- def self.find_or_create_from_auth_hash(auth_hash) 
-   user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
-    user.update(
-      name: auth_hash.info.nickname,
-      profile_image: auth_hash.info.image,
-      token: auth_hash.credentials.token,
-      secret: auth_hash.credentials.secret
-    )
-    user
+  validates :email, presence: :true
+  validates :email, uniquness: :true
+
+ def self.find_or_create_by_omniauth(auth_hash)
+    self.where(:email => auth_hash.["info"]["email"]).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end 
   end
+end
+   # 
   # Will sessions be able to use this? 
 
 # #fix
 #   def name(first_name, last_name)
 #     {first_name} + {last_name}
 #   end 
-private
-  def auth_hash
-    request.env['omniauth.auth']
-  end 
-end
+
+
+   
+   # if user = User.find_by(:email => oauth_email)
+   #   return user
+   # else
+   #  user = User.create(:email => oauth_email, :password => SecureRandom.hex)
+  
+   #  user.update(
+   #    name: auth_hash.info.nickname,
+   #    profile_image: auth_hash.info.image,
+   #    token: auth_hash.credentials.token,
+   #    secret: auth_hash.credentials.secret
+   #  )
+   #  user
+# private
+#   def omni_hash
+#     request.env['omniauth.auth']
+#   end 
+# end
